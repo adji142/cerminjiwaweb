9a568f1e4dea57ae12921b6a1601f3ef46700aa0
adji142 <prasetyoajiw@gmail.com>
2020-05-10 23:56:28 +0700

    minor changes

--- application/views/post.php
+++ application/views/post.php
@@ -127,7 +127,7 @@
 
 	        $.ajax({
 	          type: "post",
-	          url: "<?=base_url()?>c_post/read",
+	          url: "<?=base_url()?>c_post/Read",
 	          data: {where_field:where_field},
 	          dataType: "json",
 	          success: function (response) {
