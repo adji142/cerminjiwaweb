ad707dc7000ecf2d3793909d30efccb2b0e9c302
adji142 <prasetyoajiw@gmail.com>
2020-06-01 15:42:57 +0700

    Try to release 3

--- application/controllers/c_post.php
+++ application/controllers/c_post.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class C_post extends CI_Controller {
+class c_post extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
